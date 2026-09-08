6baee0bd20f8959ec8fd9ef3f5c5f641602e6d40
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-09-08 20:18:09 +0300

    Refactor database query arguments for improved readability and maintainability

--- src/db/database.js
+++ src/db/database.js
@@ -67,7 +67,13 @@ const insertLog = ({sourceIp, sourcePort, targetPort, protocol}) => {
         VALUES (@sourceIp, @sourcePort, @targetPort, @protocol);
     `);
 
-    const result = prepareLogTable.run({sourceIp, sourcePort, targetPort, protocol});
+    const result = prepareLogTable.run({
+        sourceIp,
+        sourcePort,
+        targetPort,
+        protocol
+    });
+
     return result.lastInsertRowid;
 }
 
@@ -79,7 +85,13 @@ export const addTelnetLog = ({sourceIp, sourcePort, targetPort, attemptedUsernam
         VALUES (@id, @attemptedUsername, @attemptedPassword, @commandsExecuted, @rawPayload);
     `);
 
-    return query.run({id, attemptedUsername, attemptedPassword, commandsExecuted, rawPayload});
+    return query.run({
+        id,
+        attemptedUsername,
+        attemptedPassword,
+        commandsExecuted,
+        rawPayload
+    });
 }
 
 export const addHttpLog = ({sourceIp, sourcePort, targetPort, method, headers, bodyPayload, url, responseStatus}) => {
@@ -90,7 +102,14 @@ export const addHttpLog = ({sourceIp, sourcePort, targetPort, method, headers, b
         VALUES (@id, @method, @headers, @bodyPayload, @url, @responseStatus)
     `);
 
-    return query.run({id, method, headers, bodyPayload, url, responseStatus});
+    return query.run({
+        id,
+        method,
+        headers,
+        bodyPayload,
+        url,
+        responseStatus
+    });
 }
 
 export const addSshLog = ({sourceIp, sourcePort, targetPort, attemptedUsername, attemptedPassword, clientVersion, method, publicKeyFingerprint, rawPayload}) => {
